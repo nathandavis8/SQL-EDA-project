597529d58c2e33c88cc0a3220f91934c2441be90
Nathan Rosado <nathand.rosado@gmail.com>
2025-05-04 09:09:04 -0700

    New folder added for Scripts

---
+++ Scripts/08_cumulative_analysis.sql
@@ -0,0 +1,61 @@
+/*
+===============================================================================
+Cumulative Analysis
+===============================================================================
+Purpose:
+    - To calculate running totals or moving averages for key metrics.
+    - To track performance over time cumulatively.
+    - Useful for growth analysis or identifying long-term trends.
+
+SQL Functions Used:
+    - Window Functions: SUM() OVER(), AVG() OVER()
+===============================================================================
+*/
+
+-- Calculate the total sales per month
+-- and the running total of sales over time
+
+-- Total monthly sales
+SELECT 
+FORMAT(order_date, 'yyyy-MMM') AS order_date,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM');
+
+-- Total sales by month and the running total of sales by year 
+SELECT
+order_date,
+total_sales,
+SUM(total_sales) OVER (PARTITION BY YEAR(order_date) ORDER BY order_date) AS running_total_sales
+FROM
+(
+SELECT
+DATETRUNC(month, order_date) AS order_date,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+) t
+
+-- Total sales by year and the running total of sales over time 
+SELECT
+FORMAT(order_date, 'yyyy') AS order_year,
+total_sales,
+SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
+AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
+FROM
+(
+SELECT
+DATETRUNC(year, order_date) AS order_date,
+SUM(sales_amount) AS total_sales,
+AVG(price) AS avg_price
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(year, order_date)
+) t;
+
+
